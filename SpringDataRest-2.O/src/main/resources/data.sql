f5fdd466ab87ef320ec36ce71154eec57e05b67c
Aman Kumar <amankumarmaurya93@gmail.com>
2025-11-12 23:47:50 +0530

    Full working Spring Boot project using Spring Data REST (no controller required)

---
+++ SpringDataRest-2.O/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into animal values (101, 'Aman', 'Spring Boot'), (102, 'Pirate', 'Russian'),
+(103, 'Harsh', 'C++'), (104, 'Shruti', 'Java'), (105, 'Rishabh', 'Java'), (106, 'Avin', 'Apple'), (107, 'Jack', 'Oracle'),
+(108, 'Kundan', 'Russian');
